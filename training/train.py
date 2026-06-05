3c7ce225af1dfcb40484a3bcf792db084062ce39
wout <wout.verstraete2@student.howest.be>
2026-06-05 18:04:21 +0200

    Trigger retraining pipeline run

--- training/train.py
+++ training/train.py
@@ -18,6 +18,7 @@ DataCollatorForSeq2Seq = None
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Trigger cloud retraining pipeline
 PROJECT_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = PROJECT_DIR / "data"
 
